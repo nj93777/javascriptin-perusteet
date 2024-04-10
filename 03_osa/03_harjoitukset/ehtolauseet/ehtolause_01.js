79f9523dd5e3a6ab495204c188d8aa115f1a60bd
nj93777 <nurmi.johnny@protonmail.com>
2024-04-10 19:05:58 +0300

    tulee nannaa

--- 03_osa/03_harjoitukset/ehtolauseet/ehtolause_01.js
+++ 03_osa/03_harjoitukset/ehtolauseet/ehtolause_01.js
@@ -3,7 +3,12 @@ Toteuta funktio canVote siten, että se palauttaa arvon true aina kun ikä on 18
 */
 
 function canVote(age) {
-
+    // 18 tai enemmän
+    if (age >= 18) {
+        return true; 
+    } else {
+        return false; 
+    }
 }
 
 // Esimerkkikäyttö - älä muuta
@@ -15,7 +20,18 @@ console.log(canVote(10)); // false
 
 // Treenaa: Keksi itse samanlainen funktio eri nimellä
 
-
-
+function canBuy(age) {
+    // 21 tai enemmän
+if (age >=21) {
+    return true;
+} else {
+    return false
+}
+}
 
 // Kutsu tekemääsi funktiota
+
+console.log(canBuy(30)); // true
+console.log(canBuy(21)); // true
+console.log(canBuy(3)); // false
+console.log(canBuy(19)); // false
