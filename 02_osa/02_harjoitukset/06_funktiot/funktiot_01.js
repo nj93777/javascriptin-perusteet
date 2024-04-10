76f7b055050e0e2d0ee8956d6f19ba2cfe0b1a8e
nj93777 <nurmi.johnny@protonmail.com>
2024-04-10 14:00:57 +0300

    lisaa

--- 02_osa/02_harjoitukset/06_funktiot/funktiot_01.js
+++ 02_osa/02_harjoitukset/06_funktiot/funktiot_01.js
@@ -1,6 +1,11 @@
 // Kirjoita funktio sum siten, että se palauttaa a:n ja b:n summan.
 
+function sum(a, b) {
+    // Palauttaa kahden argumentin summan
+    return a + b;
+  }
+  
 
 // Esimerkki - älä muokkaa
-console.log(sum(1, 3, 5));
+console.log(sum(1, 3, 5)); //vain kaksi ensimmäistä numeroa lasketaan
 console.log(sum(2, 5));
