c16c6c7d83c3306adc46de37ca630d7995fa8ff4
nj93777 <nurmi.johnny@protonmail.com>
2024-04-10 14:03:01 +0300

    hhh

--- 02_osa/02_harjoitukset/06_funktiot/funktiot_02.js
+++ 02_osa/02_harjoitukset/06_funktiot/funktiot_02.js
@@ -1,5 +1,9 @@
 // Kirjoita funktio multiply siten, että se palauttaa a:n ja b:n tulon (kertolaskun tulos).
 
+function multiply(a, b) {
+    // Palauttaa kahden luvun tulon
+    return a * b;
+  }
 
 // Esimerkkikäyttö - älä muokkaa
 console.log(multiply(2, 4));
