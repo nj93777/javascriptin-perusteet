9a755e1c8f97f4daddccf9d45156cce9306561e0
nj93777 <nurmi.johnny@protonmail.com>
2024-04-10 11:50:11 +0300

    up

--- 02_osa/02_harjoitukset/05_merkkijonot/merkkijonot_01.js
+++ 02_osa/02_harjoitukset/05_merkkijonot/merkkijonot_01.js
@@ -3,19 +3,48 @@ Täydennä funktio getCharCount niin, että se palauttaa argumenttina str saamie
 */
 
 function getCharCount(str) {
-  
+     // Oletetaan, että str on aina merkkijono, joten palautetaan suoraan sen pituus
+  return str.length;
 }
+
 
 // Esimerkkikäyttö - älä muokkaa
 console.log(getCharCount('Sam')); // 3
 console.log(getCharCount('Alex 123')); // 8
-console.log(getCharCount('Jimi was here')); // 15
-
+console.log(getCharCount('Jimi was here')); // 15 (oikea luku on 14)
 
 
 // Treenaa: Keksi itse samanlainen funktio eri nimellä
 
 
+function countWords(str) {
+    // Poistetaan ylimääräiset välilyönnit ja erikoismerkit, erotellaan sitten sanat
+    const words = str.replace(/[^\w\s]|_/g, "")
+                     .replace(/\s+/g, " ")
+                     .trim()
+                     .split(' ');
 
+    // Palautetaan sanojen määrä
+    return words.length;
+}
 
 // Kutsu tekemääsi funktiota
+console.log(countWords('Hei miten menee')); // 3
+console.log(countWords('En keksi mitään eli tämä on esimerkki lause')); // 8
+console.log(countWords('JavaScript ohjelmointi...:(')); // 2
+
+
+// TREENI2
+
+function countLetters(str) {
+    // Poistetaan kaikki muu paitsi kirjaimet, mukaan lukien ääkköset
+    const lettersOnly = str.replace(/[^a-zA-ZöåÖÅäÄ]/g, "");
+
+    // Palautetaan kirjainten määrä
+    return lettersOnly.length;
+}
+
+// Kutsu tekemääsi funktiota
+console.log(countLetters('Hei miten menee')); // 13
+console.log(countLetters('En keksi mitään eli tämä on esimerkki lause')); // 36
+console.log(countLetters('JavaScript ohjelmointi...:(')); // 21
--- 02_osa/02_harjoitukset/05_merkkijonot/merkkijonot_02.js
+++ 02_osa/02_harjoitukset/05_merkkijonot/merkkijonot_02.js
@@ -3,7 +3,8 @@ Muokkaa funktio shoutMyName palauttamaan saamansa name-parametri isolla kirjaimi
  */
 
 function shoutMyName(name) {
-
+// Muuttaa ja palauttaa nimen ISOILLA kirjaimilla
+return name.toUpperCase();
 }
 
 // Esimerkki - älä muokkaa
@@ -15,7 +16,17 @@ console.log(shoutMyName('alex')); // "ALEX"
 
 // Treenaa: Keksi itse samanlainen funktio eri nimellä
 
+//Palauttaa nimet pienillä kirjaimilla.
 
-
+function namesToLowerCase(name) {
+    // Muuttaa ja palauttaa nimen pienillä kirjaimilla
+    return name.toLowerCase();
+}
 
 // Kutsu tekemääsi funktiota
+console.log(namesToLowerCase('ApplE')); // "apple"
+console.log(namesToLowerCase('NoKiA')); // "nokia"
+console.log(namesToLowerCase('koira')); // "koira"
+
+
+
