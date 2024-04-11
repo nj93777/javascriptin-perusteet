f186fc9e199fe329c18be58cd80fce00c4707478
nj93777 <nurmi.johnny@protonmail.com>
2024-04-11 13:02:14 +0300

    up

--- 03_osa/03_harjoitukset/ehtolauseet/ehtolause_05.js
+++ 03_osa/03_harjoitukset/ehtolauseet/ehtolause_05.js
@@ -3,17 +3,34 @@ Täydennä funktio evenOrOdd siten, että se palauttaa merkkijonon "even", kun s
  */
 
 function evenOrOdd(number) {
-  
+    //tarkista onko parillinen luku
+    if (number % 2 ===0) {
+        return "even";
+} else {
+    return "odd";
 }
+  }
 
 // Esimerkkikäyttö - älä muokkaa
 console.log(evenOrOdd(25)); // "odd"
 console.log(evenOrOdd(18)); // "even"
 console.log(evenOrOdd(-17)); // "odd"
 
+
 // Treenaa: Keksi itse samanlainen funktio eri nimellä
 
+//palauta merkkijonon "black" kun se saa parametriksi parillisen, muuten "red".
 
-
+function even0rred(number) {
+if (number % 2 ===0) {
+    return "black";
+} else {
+        return "red";
+    }
+}
 
 // Kutsu tekemääsi funktiota
+
+console.log(even0rred(25)); //red
+console.log(even0rred(17)); //red
+console.log(even0rred(12)); //black
--- 03_osa/03_harjoitukset/ehtolauseet/ehtolause_07.js
+++ 03_osa/03_harjoitukset/ehtolauseet/ehtolause_07.js
@@ -11,6 +11,20 @@ Palauttaa kirjainarvosanan pistemäärän perusteella seuraavasti:
 
 */
 
+function assignGrade(score) {
+    if (score >= 90){
+        return 'A';
+    } else if (score >= 80){
+    return 'B';    
+    } else if (score >= 70){
+        return 'C';
+    } else if (score >= 60) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+}
+
 // Esimerkkikäyttö - älä muokkaa
 console.log('Sinä sait ' + assignGrade(95)); // Sinä sait A
 console.log('Sinä sait ' + assignGrade(81)); // Sinä sait B
@@ -22,7 +36,29 @@ console.log('Sinä sait ' + assignGrade(42)); // Sinä sait F
 
 // Treenaa: Keksi itse samanlainen funktio eri nimellä. Pisterajoja voi olla vähemmän kuin yllä.
 
+/**Palauttaa kommentin pistemäärän perusteella
+- 'Olet ässä!' pistemäärille 90 tai enemmän.
+- 'Tosi hyvä!' pistemäärille 70-89.
+- 'Paremmin voisi mennä' pistemäärille 49-69. 
+- 'Yritä uudelleen'
+*/
 
 
+
+function assignComments(score) {
+    if (score >= 90) {
+        return 'Olet ässä!';
+    } else if (score >= 70) {
+        return 'Tosi hyvä!';
+    } else if (score >= 49) {
+        return 'Paremmin voisi mennä';
+    } else {
+        return 'Yritä uudelleen';
+    }
+}
 
 // Kutsu tekemääsi funktiota
+console.log('Arvio: ' + assignComments(95)); // Arvio: Olet ässä!
+console.log('Arvio: ' + assignComments(81)); // Arvio: Tosi hyvä!
+console.log('Arvio: ' + assignComments(69)); // Arvio: Paremmin voisi mennä
+console.log('Arvio: ' + assignComments(48)); // Arvio: Yritä uudelleen
