4e76d33de2cd836992768f8aa5f90fbf74b04121
nj93777 <nurmi.johnny@protonmail.com>
2024-04-11 14:46:28 +0300

    11.4.vikat

--- 03_osa/03_harjoitukset/silmukat/silmukat_01.js
+++ 03_osa/03_harjoitukset/silmukat/silmukat_01.js
@@ -1,1 +1,8 @@
-//Tee ohjelma, joka tulostaa kaikki parittomat numerot 1 ja 100 välillä. Eli 1, 3, 5, 7, 9, 11 jne. 
+//Tee ohjelma, joka tulostaa kaikki parittomat numerot 1 ja 100 välillä. Eli 1, 3, 5, 7, 9, 11 jne.
+
+
+for (let i = 1; i <= 100; i++) {
+    if (i % 2 !== 0) {
+        console.log(i);
+    }
+}
--- 03_osa/03_harjoitukset/silmukat/silmukat_02.js
+++ 03_osa/03_harjoitukset/silmukat/silmukat_02.js
@@ -1,2 +1,16 @@
 //Tee ohjelma, joka tulostaa kaikki parilliset kokonaisluvut 1 ja 99 välillä, esittäen ne seuraavalla tavalla
-//2, 98, 4, 96, 6, 62 ja niin edelleen. Tulosta luvut samalle riville. 
+//2, 98, 4, 96, 6, 94 ja niin edelleen. Tulosta luvut samalle riville. 
+
+let kasvava = 2, vähenevä = 98;
+
+while (kasvava <= vähenevä) {
+    // Tulosta kasvava luku ja vähenevä luku
+    if (kasvava != 2) {
+        process.stdout.write(", ");
+    }
+    process.stdout.write(kasvava + ", " + vähenevä);
+
+    // Päivitä luvut seuraavaa iteraatiota varten
+    kasvava += 2;
+    vähenevä -= 2;
+}
